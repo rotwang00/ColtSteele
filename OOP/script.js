f1b45c6b95f3e5e96b04005cb4f7c6c283dbaa99
Paul Thurmond <paulthurmond@gmail.com>
2024-01-20 08:15:42 -0600

    OOP

--- OOP/script.js
+++ OOP/script.js
@@ -1,55 +1,10 @@
-// function hex(r, g, b) {
-//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-// }
-
-// function rgb(r, g, b) {
-//   return `rgb(${r}, ${g}, ${b})`;
-// }
-
-// function makeColor(r, g, b) {
-//   const color = {};
-//   color.r = r;
-//   color.g = g;
-//   color.b = b;
-//   color.rgb = function () {
-//     return `rgb(${r}, ${g}, ${b})`;
-//   };
-//   color.hex = function () {
-//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-//   };
-//   return color;
-// }
-
-// function Color(r, g, b) {
-//   this.r = r;
-//   this.g = g;
-//   this.b = b;
-// }
-
-// Color.prototype.rgb = function () {
-//   const { r, g, b } = this;
-//   return `rgb(${r}, ${g}, ${b})`;
-// };
-
-// Color.prototype.hex = function () {
-//   const { r, g, b } = this;
-//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-// };
-
-// Color.prototype.rgba = function (a = 1.0) {
-//   const { r, g, b } = this;
-//   return `rgba(${r}, ${g}, ${b}, ${a})`;
-// };
-
-// const color1 = new Color(255, 255, 2);
-// const color2 = new Color(0, 200, 160);
-
 class Color {
   constructor(r, g, b, name) {
     this.r = r;
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
   }
   greet() {
     return `Hello from ${this.name}!`;
@@ -74,7 +29,55 @@ class Color {
     const { r, g, b } = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
+
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+
+  fullSaturation() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${100}%, ${l}%)`;
+  }
+
+  opposite() {
+    const { h, s, l } = this;
+    let oppositeH = (h + 180) % 360;
+    return `hsl(${oppositeH}, ${s}%, ${l}%)`;
+  }
+
+  calcHSL() {
+    let { r, g, b } = this;
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+    if (delta == 0) h = 0;
+    else if (cmax == r) h = ((g - b) / delta) % 6;
+    else if ((cmax = g)) h = (b - r) / delta + 2;
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+    if (h < 0) h += 360;
+
+    l = (cmax - cmin) / 2;
+
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
 }
 
-const c1 = new Color(13, 44, 200, "random1");
-console.log(c1.rgba(0.5));
+const red = new Color(255, 67, 89, "tomato");
+const white = new Color(255, 255, 255, "white");
